3dd665f5eaf92ad2c0b09382d6b5363bf91267a9
Usama Shafiq <usama.shafiq@techlogix.com>
2024-12-16 11:13:47 +0500

    Initial Commit

---
+++ NetflixSwiftUI/NetflixSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixSwiftUIApp.swift
+//  NetflixSwiftUI
+//
+//  Created by UsamaShafiq on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
